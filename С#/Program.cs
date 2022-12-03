fb697ef801e31288d492286c9b2f417b7c94c8b0
Fortunella <sherbakovdaniil6@gmail.com>
2022-12-04 01:04:16 +0300

    add C# code

---
+++ С#/Program.cs
@@ -0,0 +1,38 @@
+﻿void PrintArray(string[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        if (matrix[i] == null) return;
+        Console.Write($"{matrix[i]} ");
+    }
+}
+
+
+string[] GetNewArray(string[] arr)
+{
+    string[] newArr = new string[arr.Length];
+    int pos = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i].Length <= 3)
+        {
+            newArr[pos] = arr[i];
+            pos++;
+        }
+    return newArr;
+
+}
+
+
+Console.WriteLine("Введите количество элементов:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива:");
+    array[i] = Console.ReadLine();
+}
+
+string[] newArray = GetNewArray(array);
+PrintArray(newArray);
